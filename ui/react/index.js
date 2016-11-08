f4974118cc5e6772fdbc9e952eb5aa2fa13c0c4b
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-11-08 11:19:52 +0200

    feat: add local provider to ut-rule routes

--- ui/react/index.js
+++ ui/react/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Route, Redirect, IndexRoute } from 'react-router';
 import { Main } from './pages';
 import { getRoute } from 'ut-front/react/routerHelper';
 import registerRoutes from './registerRoutes';
 export const mainRoute = registerRoutes();
-export const UtRuleRoutes = () => {
+const getLocalProvider = (config) => {
+    return React.createClass({
+        propTypes: {
+            children: PropTypes.object
+        },
+        childContextTypes: {
+            config: PropTypes.object
+        },
+        getChildContext() {
+            return {
+                config: config
+            };
+        },
+        render() {
+            return this.props.children;
+        }
+    });
+};
+
+export const UtRuleRoutes = (config) => {
     return (
-        <Route>
+        <Route component={getLocalProvider(config)}>
             <Redirect from='/' to={getRoute('ut-rule:home')} />
             <Route path={getRoute('ut-rule:home')}>
                 <IndexRoute component={Main} />

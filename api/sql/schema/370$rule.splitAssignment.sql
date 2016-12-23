26cd51969d813e1bb3040c696b7d9ffd7e9ca760
stamen.peev <stamen.peev@softwaregroup-bg.com>
2016-12-23 11:34:25 +0200

    feat: add tables splitRange, splitAssigment and splitName

---
+++ api/sql/schema/370$rule.splitAssignment.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [rule].[splitAssignment] (
+    splitAssignmentId INT IDENTITY NOT NULL,
+    splitNameId INT NOT NULL,
+    debit VARCHAR(50) NOT NULL,
+    credit VARCHAR(50) NOT NULL,
+    minValue MONEY,
+    maxValue MONEY,
+    [percent] DECIMAL,
+    description VARCHAR(50) NOT NULL,
+    CONSTRAINT [pkRuleSplitAssignment] PRIMARY KEY CLUSTERED (splitAssignmentId ASC),
+    CONSTRAINT [fkRuleSplitAssignment_ruleSplitName] FOREIGN KEY (splitNameId) REFERENCES [rule].[splitName](splitNameId)
+)

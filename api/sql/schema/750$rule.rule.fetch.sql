7c1390b1b43539d74dd706b665f5fbd13366ffae
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-01-10 18:54:55 +0200

    fix: fix rule.rule.add according to new redesign

--- api/sql/schema/750$rule.rule.fetch.sql
+++ api/sql/schema/750$rule.rule.fetch.sql
@@ -3,7 +3,6 @@ ALTER PROCEDURE [rule].[rule.fetch]
 AS
 BEGIN
     SELECT 'condition' AS resultSetName
-
     SELECT
         *
     FROM
@@ -11,30 +10,40 @@ BEGIN
     WHERE
         @conditionId IS NULL OR conditionId = @conditionId
 
+    SELECT 'splitName' AS resultSetName
+    SELECT
+        *
+    FROM
+        [rule].splitName
+    WHERE
+        @conditionId IS NULL OR conditionId = @conditionId
+
+    SELECT 'splitRange' AS resultSetName
+    SELECT
+        sr.*
+    FROM
+        [rule].splitRange sr
+    JOIN
+        [rule].splitName sn ON sn.splitNameId = sr.splitNameId
+    WHERE
+        @conditionId IS NULL OR sn.conditionId = @conditionId
+
+    SELECT 'splitAssignment' AS resultSetName
+    SELECT
+        sa.*
+    FROM
+        [rule].splitAssignment sa
+    JOIN
+        [rule].splitName sn ON sn.splitNameId = sa.splitNameId
+    WHERE
+        @conditionId IS NULL OR sn.conditionId = @conditionId
+
     SELECT 'limit' AS resultSetName
 
     SELECT
         *
     FROM
         [rule].limit
-    WHERE
-        @conditionId IS NULL OR conditionId = @conditionId
-
-    SELECT 'commission' AS resultSetName
-
-    SELECT
-        *
-    FROM
-        [rule].commission
-    WHERE
-        @conditionId IS NULL OR conditionId = @conditionId
-
-    SELECT 'fee' AS resultSetName
-
-    SELECT
-        *
-    FROM
-        [rule].fee
     WHERE
         @conditionId IS NULL OR conditionId = @conditionId
 END

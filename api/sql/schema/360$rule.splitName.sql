5e16406f38376fef405979edceffff974daa02f1
stamen.peev <stamen.peev@softwaregroup-bg.com>
2017-01-11 14:37:49 +0200

    fix: add tag column in rule.splitName

--- api/sql/schema/360$rule.splitName.sql
+++ api/sql/schema/360$rule.splitName.sql
@@ -2,6 +2,7 @@ CREATE TABLE [rule].[splitName] (
     splitNameId INT IDENTITY NOT NULL,
     conditionId INT NOT NULL,
     name VARCHAR(50) NOT NULL,
+    tag VARCHAR(max),
     CONSTRAINT [pkRuleSplitName] PRIMARY KEY CLUSTERED (splitNameId ASC),
     CONSTRAINT [fkRuleSplitName_ruleCondition] FOREIGN KEY (conditionId) REFERENCES [rule].[condition](conditionId),
     CONSTRAINT [ukRuleSplitNameConditionIdName] UNIQUE (conditionId, name)

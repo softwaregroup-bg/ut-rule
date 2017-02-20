eb041e982398a9b5b2c70a195c95e6f72ef19528
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-02-20 14:32:16 +0200

    fix: condition with operation tag

--- api/sql/schema/750$rule.decision.fetch.sql
+++ api/sql/schema/750$rule.decision.fetch.sql
@@ -74,7 +74,7 @@ BEGIN
         (@channelRoleId IS NULL OR c.channelRoleId IS NULL OR @channelRoleId = c.channelRoleId) AND
         (@channelId IS NULL OR c.channelId IS NULL OR @channelId = c.channelId) AND
         (@operationId IS NULL OR c.operationId IS NULL OR @operationId = c.operationId) AND
-        (c.operationTag IS NULL OR (@operationTag IS NOT NULL AND c.operationTag LIKE '%|' + @operationTag + '|%')) AND
+        ((@operationTag IS NULL AND c.operationTag IS NULL) OR (@operationTag IS NOT NULL AND c.operationTag LIKE '%|' + @operationTag + '|%')) AND
         -- (c.operationTag IS NULL OR @operationId IS NULL OR EXISTS(SELECT * from core.itemTag t WHERE t.itemNameId = @operationId AND c.operationTag LIKE '%|' + t.tag + '|%')) AND
         (@operationDate IS NULL OR c.operationStartDate IS NULL OR (@operationDate >= c.operationStartDate)) AND
         (@operationDate IS NULL OR c.operationEndDate IS NULL OR (@operationDate <= c.operationEndDate)) AND

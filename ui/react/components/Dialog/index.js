71d47ce357e71fa3baa09ea86a3357f9181ebb5a
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-05-02 11:44:01 +0300

    fix: db name conventions

--- ui/react/components/Dialog/index.js
+++ ui/react/components/Dialog/index.js
@@ -16,6 +16,9 @@ import classnames from 'classnames';
 import set from 'lodash.set';
 
 function capitalizeFirstLetter(string) {
+    if (!string) {
+        return string;
+    }
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 const emptyCondition = {

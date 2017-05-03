3f6de15e4ec1e22222416a67b69974d74957d891
Kristiyan Serafimov <kristian.plamenov@gmail.com>
2017-05-03 18:04:50 +0300

    ABT, NBV and Standart implementation fix (#43)
    * fix: abt
    
    * fix: rule.rule.fetch actionCreator file

--- ui/react/components/Dialog/Section/Splits/index.js
+++ ui/react/components/Dialog/Section/Splits/index.js
@@ -51,7 +51,9 @@ const Splits = React.createClass({
             {key: 'issuer', name: 'Issuer'},
             {key: 'commission', name: 'Commission'},
             {key: 'realtime', name: 'Realtime posting'},
-            {key: 'pending', name: 'Authorization required'}
+            {key: 'pending', name: 'Authorization required'},
+            {key: 'agent', name: 'Agent'},
+            {key: 'fee', name: 'Fee'}
         ];
     },
     defaultSelected(origin, selected) {

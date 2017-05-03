d38bec1f1711ed824abab085214e5317388717c5
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-05-03 10:11:20 +0300

    fix: merge sql

--- api/sql/schema/seeds/850$permission.sql
+++ api/sql/schema/seeds/850$permission.sql
@@ -1,32 +1,35 @@
- DECLARE @ruleActionCategoryId INT = (SELECT actionCategoryId FROM [user].[actionCategory] WHERE name = 'rule')
-
-if @ruleActionCategoryId is null
-begin
-    insert into [user].[actionCategory](name)
-    values ('rule')
-
-    set @ruleActionCategoryId = SCOPE_IDENTITY()
-end
-
-declare @action [user].[actionTT]
-
-insert into @action ([actionId], [actionCategoryId], [description], [valueMap])
-
-VALUES ('rule.decision.fetch', @ruleActionCategoryId, 'rule.decision.fetch', '{}')
-    ,('rule.operation.lookup', @ruleActionCategoryId, 'rule.operation.lookup', '{}')
-    ,('rule.rule.add', @ruleActionCategoryId, 'rule.rule.add', '{}')
-    ,('rule.rule.edit', @ruleActionCategoryId, 'rule.rule.edit', '{}')
-    ,('rule.rule.fetch', @ruleActionCategoryId, 'rule.rule.fetch', '{}')
-    ,('rule.rule.remove', @ruleActionCategoryId, 'rule.rule.remove', '{}')
-    ,('rule.item.fetch', @ruleActionCategoryId, 'rule.item.fetch', '{}')
-
-;MERGE into [user].[action] as dd
-using
-(
-    select [actionId], [actionCategoryId], [description], [valueMap]
-    from @action
-) d on dd.actionId=d.actionId
-
+MERGE INTO
+    [user].[actionCategory] as target
+USING
+    (VALUES
+        ('rule')
+    ) AS source (name)
+ON
+    target.name=source.name
 WHEN NOT MATCHED BY TARGET THEN
-    INSERT ([actionId], [actionCategoryId], [description], [valueMap])
-    VALUES ([actionId], [actionCategoryId], [description], [valueMap]);
+INSERT
+    ([name])
+VALUES
+    (source.[name]);
+
+MERGE INTO
+    [user].[action] as target
+USING
+    (VALUES
+        ('rule.decision.fetch', 'rule.decision.fetch', '{}')
+        ,('rule.operation.lookup', 'rule.operation.lookup', '{}')
+        ,('rule.rule.add', 'rule.rule.add', '{}')
+        ,('rule.rule.edit', 'rule.rule.edit', '{}')
+        ,('rule.rule.fetch', 'rule.rule.fetch', '{}')
+        ,('rule.rule.remove', 'rule.rule.remove', '{}')
+        ,('rule.item.fetch', 'rule.item.fetch', '{}')
+    ) AS source (actionId, description, valueMap)
+JOIN
+    [user].[actionCategory] c ON c.name = 'rule'
+ON
+    target.actionId=source.actionId
+WHEN NOT MATCHED BY TARGET THEN
+INSERT
+    ([actionId], [actionCategoryId], [description], [valueMap])
+VALUES
+    (source.[actionId], c.[actionCategoryId], source.[description], source.[valueMap]);

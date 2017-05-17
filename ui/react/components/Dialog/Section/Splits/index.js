a4fa2d2924c209c5e89b6c1082dc104987137be8
Ventsislav Arsov <Ventsislav.Arsov@Softwaregroup-bg.com>
2017-05-17 11:52:19 +0300

    fix: add additional tags in frontend

--- ui/react/components/Dialog/Section/Splits/index.js
+++ ui/react/components/Dialog/Section/Splits/index.js
@@ -55,7 +55,10 @@ const Splits = React.createClass({
             {key: 'realtime', name: 'Realtime posting'},
             {key: 'pending', name: 'Authorization required'},
             {key: 'agent', name: 'Agent'},
-            {key: 'fee', name: 'Fee'}
+            {key: 'fee', name: 'Fee'},
+            {key: 'atm', name: 'Atm'},
+            {key: 'pos', name: 'Pos'},
+            {key: 'ped', name: 'Ped'}
         ];
     },
     defaultSelected(origin, selected) {

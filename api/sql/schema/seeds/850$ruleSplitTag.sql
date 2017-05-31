0ac0f18103862b3296f63e26dc19f6a5f3ca2ee1
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-05-31 17:48:36 +0300

    feat: add transalation for rule split tag

--- api/sql/schema/seeds/850$ruleSplitTag.sql
+++ api/sql/schema/seeds/850$ruleSplitTag.sql
@@ -28,10 +28,41 @@ USING
 JOIN
 	core.itemType t on t.alias='ruleSplitTag'
 ON
-    target.itemCode = source.itemCode
+    target.itemCode = source.itemCode AND target.itemTypeId = t.itemTypeId 
 WHEN
     NOT MATCHED BY TARGET THEN
 INSERT
     (itemTypeId, itemCode, itemName)
 VALUES
     (t.itemTypeId, source.itemCode, source.itemName);
+
+/*******************************Trnaslation***********************************************/
+DECLARE @itemNameTranslationTT core.itemNameTranslationTT
+DECLARE @languageId bigint = (select languageId from core.language where iso2Code = 'en')
+
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Acquirer', 'acquirer', N'Acquirer')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Issuer', 'issuer',N'Issuer')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Commission', 'commission',N'Commission')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Realtime posting', 'realtime',N'Realtime posting')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Authorization required', 'pending',N'Authorization required')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Agent', 'agent',N'Agent')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'Fee', 'fee',N'Fee')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'ATM', 'atm', N'ATM')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'POS', 'pos', N'POS')
+INSERT INTO @itemNameTranslationTT(itemSyncId, itemName, itemCode, itemNameTranslation) VALUES (NULL, 'PED', 'ped', N'PED')
+
+
+MERGE [core].[itemTranslation] AS t
+USING
+(
+    SELECT @languageid, r.itemNameId, tt.itemNameTranslation
+    FROM @itemNameTranslationTT tt
+    JOIN core.itemName r ON r.itemcode = tt.itemCode AND r.itemTypeId = @itemTypeId
+) AS s (languageId, itemNameId , itemNameTranslation)
+ON (t.languageId = s.languageId and t.itemNameId = s.itemNameId )
+WHEN MATCHED THEN
+	   UPDATE
+    SET itemNameTranslation = s.itemNameTranslation
+WHEN NOT MATCHED BY TARGET THEN
+	   INSERT (languageId, itemNameId, itemNameTranslation)
+	   VALUES (s.languageId, s.itemNameId, s.itemNameTranslation);

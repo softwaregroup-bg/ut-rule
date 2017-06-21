a629519e8ce3d2b44716f1ba86f4b7496e96fa74
Dimitar Ivanov <dimitar.ivanov@softwaregroup-bg.com>
2017-06-21 12:47:40 +0300

    fix: Display actorName in grid or fallback to actorId

--- ui/react/pages/Main/reducer.js
+++ ui/react/pages/Main/reducer.js
@@ -155,7 +155,7 @@ const getFormattedGridDataColumns = function(fetchedData, formattedRules) {
 
         result[actor.conditionId][actor.factor].push({
             name: actor.type,
-            value: actor.actorId
+            value: actor.actorName || actor.actorId
         });
     });
     Object.keys(formattedRules).forEach((conditionId) => {

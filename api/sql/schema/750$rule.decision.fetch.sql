f4710023e3640a46a605195875b5ed801148c8da
zetxx <zetxx@belogradcik.biz>
2017-11-30 20:41:35 +0200

    fix: lots of typos

--- api/sql/schema/750$rule.decision.fetch.sql
+++ api/sql/schema/750$rule.decision.fetch.sql
@@ -152,17 +152,17 @@ BEGIN
     BEGIN
         DECLARE @type VARCHAR (20)= CASE WHEN ISNULL(@limitCredentials, 0) = 0 THEN 'rule.exceed' ELSE 'rule.unauthorized' END
         DECLARE @error VARCHAR (50) = CASE
-            WHEN @amount > @maxAmount THEN @type + 'MaxLimitAmount'
-            WHEN @amount < @minAmount THEN @type + 'MinLimitAmount'
+            WHEN @amount > @maxAmount THEN @type + 'exceedMaxLimitAmount'
+            WHEN @amount < @minAmount THEN @type + 'exceedMinLimitAmount'
             WHEN @amountDaily >= @maxAmountDaily THEN 'reachedDailyLimitAmount'
             WHEN @amountWeekly >= @maxAmountWeekly THEN 'reachedWeeklyLimitAmount'
             WHEN @amountMonthly >= @maxAmountMonthly THEN 'reachedMonthlyLimitAmount'
-            WHEN @amount + @amountDaily > @maxAmountDaily THEN @type + 'DailyLimitAmount'
-            WHEN @amount + @amountWeekly > @maxAmountWeekly THEN @type + 'WeeklyLimitAmount'
-            WHEN @amount + @amountMonthly > @maxAmountMonthly THEN @type + 'MonthlyLimitAmount'
-            WHEN @countDaily >= @maxCountDaily THEN @type + 'DailyLimitCount'
-            WHEN @countWeekly >= @maxCountWeekly THEN @type + 'WeeklyLimitCount'
-            WHEN @countMonthly >= @maxCountMonthly THEN @type + 'MonthlyLimitCount'
+            WHEN @amount + @amountDaily > @maxAmountDaily THEN @type + 'exceedDailyLimitAmount'
+            WHEN @amount + @amountWeekly > @maxAmountWeekly THEN @type + 'exceedWeeklyLimitAmount'
+            WHEN @amount + @amountMonthly > @maxAmountMonthly THEN @type + 'exceedMonthlyLimitAmount'
+            WHEN @countDaily >= @maxCountDaily THEN @type + 'exceedDailyLimitCount'
+            WHEN @countWeekly >= @maxCountWeekly THEN @type + 'exceedWeeklyLimitCount'
+            WHEN @countMonthly >= @maxCountMonthly THEN @type + 'exceedMonthlyLimitCount'
         END
 
         SELECT

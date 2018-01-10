1397fd472e89596db3a0f224bd2578734a0957de
Kristiyan Todorov <kristiyan.todorov@softwaregroup-bg.com>
2018-01-10 14:08:45 +0200

    fix: remove db prefix & refactor (#90)

---
+++ validations/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'decision.fetch': require('./decision/decision.fetch'),
+    'decision.lookup': require('./decision/decision.lookup'),
+
+    'item.fetch': require('./item/item.fetch'),
+    'rule.fetch': require('./rule/rule.fetch'),
+    'rule.add': require('./rule/rule.add'),
+    'rule.remove': require('./rule/rule.remove'),
+    'rule.edit': require('./rule/rule.edit')
+};

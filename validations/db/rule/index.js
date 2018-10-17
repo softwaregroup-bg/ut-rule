f17994aa27e91e006e39150c55aa54718d2cfd17
Bozhidar Todorov <Bozhidar.Todorov@Softwaregroup-bg.com>
2018-10-17 11:38:42 +0300

    fix: change currency

--- validations/db/rule/index.js
+++ validations/db/rule/index.js
@@ -34,7 +34,7 @@ module.exports = {
             destinationProductId: joi.number().integer().allow(null).default(null),
             destinationAccountId: joi.number().integer().allow(null).default(null),
             amount: joi.number().required().default(0),
-            currency: joi.string().length(3).required().default('ZMW'),
+            currency: joi.string().length(3).required().default('TZS'),
             isSourceAmount: joi.boolean().allow(0, 1, '0', '1')
         }).unknown(),
         result: joi.object().keys({

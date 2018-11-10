31c7c14719d4cf6365ff9677282011039cd105ef
Kalin Krustev <kalin.krustev@gmail.com>
2018-11-10 17:12:00 +0200

    feat: composable microservices

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-module.exports = () => ({
-    ports: [],
-    modules: {
-        rule: require('./api/script'),
-        'db/rule': require('./api/sql')
-    },
-    validations: {
-        rule: require('./validations')
-    },
-    errors: [
-        require('./errors')
-    ]
-});
+module.exports = () => function utRule() {
+    return [
+        function adapter() {
+            return {
+                modules: {
+                    'db/rule': require('./api/sql/schema'),
+                    ruleSeed: require('./api/sql/seed'),
+                    ruleTest: () => require('./test/schema')
+                },
+                errors: [
+                    require('./errors')
+                ]
+            };
+        },
+        function orchestrator() {
+            return {
+                ports: [
+                    require('ut-dispatch-db')(['rule'])
+                ],
+                modules: {
+                    rule: require('./api/script')
+                },
+                errors: [
+                    require('./errors')
+                ]
+            };
+        },
+        function gateway() {
+            return {
+                validations: {
+                    rule: require('./validations')
+                }
+            };
+        }
+    ];
+};
---
+++ test/schema.js
@@ -0,0 +1,4 @@
+const path = require('path');
+module.exports = {
+    schema: [{path: path.join(__dirname, 'schema'), linkSP: true}]
+};

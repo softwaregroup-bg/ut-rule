5f40e98c9204b301f970ce6bad403acce292cfbd
dimitar.neykov <dimitar.neykov@softwaregroup.com>
2020-02-12 13:48:41 +0200

    fix(UATCIB-25): PAP / Rules: Operations should be alphabetically ordered.
    - sort operation types

--- ui/react/components/Dialog/Section/Operation/index.js
+++ ui/react/components/Dialog/Section/Operation/index.js
@@ -95,7 +95,7 @@ const Operation = React.createClass({
                         name='operationIds'
                         label={fields.operationId.title}
                         value={this.props.data.operationIds}
-                        options={operation || []}
+                        options={operation.sort((a, b) => a.name.localeCompare(b.name)) || []}
                         onChange={(val) => { this.onSelectDropdown({ key: 'operationIds', value: val }); }}
                     />
                   </div>

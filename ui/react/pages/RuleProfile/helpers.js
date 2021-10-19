ee5f888d8481ddd40167b56e87d60139df0c6fd1
Gitau <francis.kinuthia@softwaregroup.com>
2021-10-19 19:10:18 +0300

    fix(PAP) remove default 0 when rendering amounts UNMB-53

--- ui/react/pages/RuleProfile/helpers.js
+++ ui/react/pages/RuleProfile/helpers.js
@@ -373,31 +373,31 @@ export const flatten = function(ob) {
 };
 
 export const formatValue = (value, sep) => {
-   if (value && !isNaN(value)){
-       const formatter = new Intl.NumberFormat('en-US', {style: 'decimal'});
-       value = `${value}`.replace(/,/g, '');
-       if (value.indexOf('.') !== -1) {
-           const grp = value.split('.');
-           if (grp[1].length > 0) {
-               grp[0] = formatter.format(grp[0]);
-               grp[1] = formatter.format(grp[1]);
-               return grp.join('.');
-           } else {
-               return formatter.format(value) + '.';
-           }
-       } else {
-           value = formatter.format(value);
-           return value;
-       }
-   }
-   return value
+    if (value && !isNaN(value)) {
+        const formatter = new Intl.NumberFormat('en-US', {style: 'decimal'});
+        value = `${value}`.replace(/,/g, '');
+        if (value.indexOf('.') !== -1) {
+            const grp = value.split('.');
+            if (grp[1].length > 0) {
+                grp[0] = formatter.format(grp[0]);
+                grp[1] = formatter.format(grp[1]);
+                return grp.join('.');
+            } else {
+                return formatter.format(value) + '.';
+            }
+        } else {
+            value = formatter.format(value);
+            return value;
+        }
+    }
+    return value;
 };
 
 export const reverseFormatter = (obj) => {
-   if(obj.value){
-       const value = isNaN(obj.value) ? obj.value :  obj.value.replace(/,/g, '');
-       obj.value = value;
-       return obj;
-   }
-   return obj
+    if (obj.value) {
+        const value = isNaN(obj.value) ? obj.value : obj.value.replace(/,/g, '');
+        obj.value = value;
+        return obj;
+    }
+    return obj;
 };

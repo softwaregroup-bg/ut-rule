c0eda1c187ed14eab531bcebddb74c132264c82b
Kalin Krustev <kalin.krustev@gmail.com>
2022-12-10 12:33:20 +0200

    chore: #UTCORE-121

--- test/seed/850-billPaymentRule.sql
+++ test/seed/850-billPaymentRule.sql
@@ -1,71 +1,74 @@
-DECLARE @condition [rule].conditionTT
-DECLARE @conditionActor [rule].conditionActorTT
-DECLARE @conditionItem [rule].conditionItemTT
-DECLARE @conditionProperty [rule].conditionPropertyTT
-DECLARE @limit [rule].limitTT
-DECLARE @split XML
-DECLARE @meta core.metaDataTT
-DECLARE @sa BIGINT = (SELECT actorId FROM [user].[hash] WHERE identifier = 0x00 /*encryptStable sa*/ AND [type] = 'password')
-INSERT INTO @meta([auth.actorId], [method]) VALUES(@sa, 'rule.rule.add')
-
-IF NOT EXISTS (SELECT * FROM [rule].condition WHERE [priority] = 100)
+IF OBJECT_ID('ledger.account', 'U') IS NOT NULL
 BEGIN
-    INSERT INTO @condition ([priority], operationStartDate, operationEndDate, sourceAccountId, destinationAccountId)
-    VALUES (100, NULL, NULL, NULL, NULL)
+    DECLARE @condition [rule].conditionTT
+    DECLARE @conditionActor [rule].conditionActorTT
+    DECLARE @conditionItem [rule].conditionItemTT
+    DECLARE @conditionProperty [rule].conditionPropertyTT
+    DECLARE @limit [rule].limitTT
+    DECLARE @split XML
+    DECLARE @meta core.metaDataTT
+    DECLARE @sa BIGINT = (SELECT actorId FROM [user].[hash] WHERE identifier = 0x00 /*encryptStable sa*/ AND [type] = 'password')
+    INSERT INTO @meta([auth.actorId], [method]) VALUES(@sa, 'rule.rule.add')
 
-    INSERT INTO @conditionItem (factor, itemNameId)
-    SELECT 'oc', itemNameId
-    FROM core.itemName cin
-    JOIN core.itemType cit ON cit.itemTypeId = cin.itemTypeId
-    WHERE cit.alias = 'operation' AND itemCode = 'bill'
+    IF NOT EXISTS (SELECT * FROM [rule].condition WHERE [priority] = 100)
+    BEGIN
+        INSERT INTO @condition ([priority], operationStartDate, operationEndDate, sourceAccountId, destinationAccountId)
+        VALUES (100, NULL, NULL, NULL, NULL)
 
-    SET @split = N'<data>
-        <rows>
-            <splitName>
-                <name>bill payment</name>
-                <tag>|realtime|</tag>
-            </splitName>
-            <splitRange>
-                <startAmount>0</startAmount>
-                <startAmountCurrency>USD</startAmountCurrency>
-                <percent>100</percent>
-                <isSourceAmount>0</isSourceAmount>
-            </splitRange>
-            <splitAssignment>
-                <debit>merchant_pool</debit>
-                <credit>${destination.account.number}</credit>
-                <percent>100</percent>
-                <description>bill payment</description>
-            </splitAssignment>
-        </rows>
-        <rows>
-            <splitName>
-                <name>bill payment fee</name>
-                <tag>|processor|fee|</tag>
-            </splitName>
-            <splitRange>
-                <startAmount>0</startAmount>
-                <startAmountCurrency>USD</startAmountCurrency>
-                <minValue>0.25</minValue>
-                <maxValue>10</maxValue>
-                <percent>0.08</percent>
-                <isSourceAmount>0</isSourceAmount>
-            </splitRange>
-            <splitAssignment>
-                <debit>${destination.account.number}</debit>
-                <credit>merchant_tax</credit>
-                <percent>100</percent>
-                <description>bill payment fee</description>
-            </splitAssignment>
-        </rows>
-    </data>'
+        INSERT INTO @conditionItem (factor, itemNameId)
+        SELECT 'oc', itemNameId
+        FROM core.itemName cin
+        JOIN core.itemType cit ON cit.itemTypeId = cin.itemTypeId
+        WHERE cit.alias = 'operation' AND itemCode = 'bill'
 
-    EXEC [rule].[rule.add]
-        @condition,
-        @conditionActor,
-        @conditionItem,
-        @conditionProperty,
-        @limit,
-        @split,
-        @meta
+        SET @split = N'<data>
+            <rows>
+                <splitName>
+                    <name>bill payment</name>
+                    <tag>|realtime|</tag>
+                </splitName>
+                <splitRange>
+                    <startAmount>0</startAmount>
+                    <startAmountCurrency>USD</startAmountCurrency>
+                    <percent>100</percent>
+                    <isSourceAmount>0</isSourceAmount>
+                </splitRange>
+                <splitAssignment>
+                    <debit>merchant_pool</debit>
+                    <credit>${destination.account.number}</credit>
+                    <percent>100</percent>
+                    <description>bill payment</description>
+                </splitAssignment>
+            </rows>
+            <rows>
+                <splitName>
+                    <name>bill payment fee</name>
+                    <tag>|processor|fee|</tag>
+                </splitName>
+                <splitRange>
+                    <startAmount>0</startAmount>
+                    <startAmountCurrency>USD</startAmountCurrency>
+                    <minValue>0.25</minValue>
+                    <maxValue>10</maxValue>
+                    <percent>0.08</percent>
+                    <isSourceAmount>0</isSourceAmount>
+                </splitRange>
+                <splitAssignment>
+                    <debit>${destination.account.number}</debit>
+                    <credit>merchant_tax</credit>
+                    <percent>100</percent>
+                    <description>bill payment fee</description>
+                </splitAssignment>
+            </rows>
+        </data>'
+
+        EXEC [rule].[rule.add]
+            @condition,
+            @conditionActor,
+            @conditionItem,
+            @conditionProperty,
+            @limit,
+            @split,
+            @meta
+    END
 END

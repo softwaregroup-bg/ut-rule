804485f581cdb98ce6923fb556ef59cfa55d4d9b
Kalin Krustev <kalin.krustev@gmail.com>
2022-12-12 10:48:45 +0200

    chore: #UTCORE-121

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = require('ut-run').microservice(module, require, () => function 
             require('./api/sql/schema'),
             require('./api/sql/seed'),
             require('./api/sql/standard'),
+            require('./test/schema'),
             require('./test/seed'),
             require('./test/unit'),
             require('./errors')

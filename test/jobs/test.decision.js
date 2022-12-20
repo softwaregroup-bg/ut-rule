30326c59fae27f8dd148a17e8a0bad618155fa55
Reni Paskaleva <reni.paskaleva@softwaregroup.com>
2022-12-20 15:22:03 +0200

    test: UTCORE-397 first changes

--- test/jobs/test.decision.js
+++ test/jobs/test.decision.js
@@ -23,12 +23,36 @@ module.exports = function test() {
                         assert.ok(result.organization[0].actorId, 'Mastercard exists');
                     }
                 },
+                {
+                    name: 'fetch countries',
+                    method: 'core.country.fetch',
+                    params: {},
+                    result: function(result, assert, {method, validation}) {
+                        assert.ok(validation[`${method}.result`], `${method} validation passed`);
+                        assert.ok(result.country, 'return fetchCountry');
+                        this.countryId = result.country.find(
+                            (country) => country.name === 'United Arab Emirates'
+                        ).countryId;
+                    }
+                },
+                {
+                    name: 'fetch city',
+                    method: 'core.itemNameByItemType.fetch',
+                    params: {itemType: 'city'},
+                    result: function(result, assert, {method, validation}) {
+                        assert.ok(validation[`${method}.result`], `${method} validation passed`);
+                        this.cityId = result.itemNameByType.find(
+                            (city) => city.itemNameTranslation === 'Aitos'
+                        ).itemNameId;
+                    }
+                },
                 ruleDecisionSnapshot({name: 'No match'}),
                 ruleDecisionSnapshot({name: 'Period', operationDate: '2022-01-10T00:00:00Z'}),
                 ruleDecisionSnapshot({name: 'Start date', operationDate: '2122-03-10T00:00:00Z'}),
                 ruleDecisionSnapshot({name: 'End date', operationDate: '2021-11-10T00:00:00Z'}),
                 ruleDecisionSnapshot({name: 'Operation', operation: 'Rule Deposit'}),
-                ruleDecisionSnapshot({name: 'Channel country', channelId: 2}),
+                ruleDecisionSnapshot({name: 'Operation tag', operationTag: 'cash'}),
+                ruleDecisionSnapshot({name: 'Channel country'}, ({countryId}) => ({channelId: countryId})),
                 ruleDecisionSnapshot({name: 'Channel region', channelId: 3}),
                 ruleDecisionSnapshot({name: 'Channel city', channelId: 4}),
                 ruleDecisionSnapshot({name: 'Channel organization'}, ({Greenpeace: {organization: [{actorId: channelId}]}}) => ({channelId})),
@@ -37,12 +61,16 @@ module.exports = function test() {
                 ruleDecisionSnapshot({name: 'Holder country', sourceAccount: 'source-country'}),
                 ruleDecisionSnapshot({name: 'Holder region', sourceAccount: 'source-region'}),
                 ruleDecisionSnapshot({name: 'Holder city', sourceAccount: 'source-city'}),
+                ruleDecisionSnapshot({name: 'Holder type', holderCustomerType: 'corporate'}),
+                ruleDecisionSnapshot({name: 'Holder kyc', holderKyc: 'Level 2 - Individual - Bulgaria, Level 3 - Corporate - Bulgaria', operation: 'Rule Withdraw'}),
                 ruleDecisionSnapshot({name: 'Holder organization', sourceAccount: 'source-organization'}),
                 ruleDecisionSnapshot({name: 'Holder organization tag', sourceAccount: 'source-organization-tag'}),
                 ruleDecisionSnapshot({name: 'Counterparty account', destinationAccount: 'destination'}),
                 ruleDecisionSnapshot({name: 'Counterparty country', destinationAccount: 'destination-country'}),
                 ruleDecisionSnapshot({name: 'Counterparty region', destinationAccount: 'destination-region'}),
                 ruleDecisionSnapshot({name: 'Counterparty city', destinationAccount: 'destination-city'}),
+                ruleDecisionSnapshot({name: 'Counterparty type', counterpartyCustomerType: 'corporate'}),
+                ruleDecisionSnapshot({name: 'Counterparty kyc', counterpartyKyc: 'Level 0 - Individual - Bulgaria', operation: 'Rule Withdraw'}),
                 ruleDecisionSnapshot({name: 'Counterparty organization', destinationAccount: 'destination-organization'}),
                 ruleDecisionSnapshot({name: 'Counterparty organization tag', destinationAccount: 'destination-organization-tag'})
             ]);
